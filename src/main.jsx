db0f8bd4d2a6f998412443ee45e5ee52355cb407
rizwanzafar7894-pixel <rizwanzafar7894@gmail.com>
2026-09-09 11:51:19 +0500

    Load enhanced blog and profile styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,2 @@
-import {StrictMode} from 'react'; import {createRoot} from 'react-dom/client'; import {BrowserRouter} from 'react-router-dom'; import App from './App'; import './styles/index.css';
+import {StrictMode} from 'react'; import {createRoot} from 'react-dom/client'; import {BrowserRouter} from 'react-router-dom'; import App from './App'; import './styles/index.css'; import './styles/blog.css';
 createRoot(document.getElementById('root')).render(<StrictMode><BrowserRouter><App/></BrowserRouter></StrictMode>);
