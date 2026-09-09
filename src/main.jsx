c9241a1979d5633819adbddc3dbfe31c37b582c9
rizwanzafar7894-pixel <rizwanzafar7894@gmail.com>
2026-09-09 21:19:01 +0500

    feat: mount global scroll progress

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,2 @@
-import {StrictMode} from 'react'; import {createRoot} from 'react-dom/client'; import {BrowserRouter} from 'react-router-dom'; import App from './App'; import './styles/index.css'; import './styles/blog.css'; import './styles/fixes.css';
-createRoot(document.getElementById('root')).render(<StrictMode><BrowserRouter><App/></BrowserRouter></StrictMode>);
+import {StrictMode} from 'react'; import {createRoot} from 'react-dom/client'; import {BrowserRouter} from 'react-router-dom'; import App from './App'; import ScrollProgress from './components/common/ScrollProgress'; import './styles/index.css'; import './styles/blog.css'; import './styles/fixes.css';
+createRoot(document.getElementById('root')).render(<StrictMode><BrowserRouter><ScrollProgress/><App/></BrowserRouter></StrictMode>);
